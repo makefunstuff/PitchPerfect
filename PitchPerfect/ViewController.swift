3d236737aae7ec59f76c269569e03c00b919348b
Iurii Plugatariov <plugatariov@gmail.com>
2018-06-07 09:01:53 +0300

    Button states; Add images

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,17 +11,32 @@ import UIKit
 class ViewController: UIViewController {
   @IBOutlet weak var recordButton: UIButton!
   @IBOutlet weak var recordingLabel: UILabel!
+  @IBOutlet weak var stopRecordingButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    stopRecordingButton.isEnabled = false
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+  }
 
   @IBAction func recordAudio(_ sender: UIButton) {
     recordingLabel.text = "Recording in Progress"
+    recordButton.isEnabled = false
+    stopRecordingButton.isEnabled = true
   }
   @IBAction func stopRecording(_ sender: UIButton) {
     recordingLabel.text = "Recording stopped"
+    stopRecordingButton.isEnabled = false
+    recordButton.isEnabled = true
   }
 }
 
